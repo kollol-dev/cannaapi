2f30466f4a42a98e0befd52a1e5c9578068411c6
nazmul <nazmulchowdhury4@gmail.com>
2019-06-25 19:13:34 +0600

    this is ok 8986

--- app/Controllers/Http/CannaGrowController.js
+++ app/Controllers/Http/CannaGrowController.js
@@ -33,7 +33,7 @@ class CannaGrowController {
     async indexItem({request,response,auth}){
         //  try {
               let user =  await auth.getUser()
-            let allItems =await Item.query().where('userId',user.id).with('tags').fetch()
+            let allItems =await Item.query().where('userId',user.id).with('tags').with('reviews').fetch()
               
               return response.status(200).json({
                   'success': true,
@@ -57,7 +57,7 @@ class CannaGrowController {
               let price2 = request.input('price2') ? request.input('price2') : ''
               let key = request.input('key') ? request.input('key') : ''
 
-              let rawData = Item.query().where('userId',7).with('tags').with('store').with('user').with('reviews')
+              let rawData = Item.query().with('tags').with('store').with('user').with('reviews')
               if(price1 && price2){
                 console.log("this is ok")
                 rawData.where('price', '>=', price1)
