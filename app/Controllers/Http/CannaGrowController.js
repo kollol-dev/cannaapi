d0f09ca2f3668e37b964e9cb52438903038994a6
nazmul <nazmulchowdhury4@gmail.com>
2019-06-25 12:05:10 +0600

    this is ok 888

--- app/Controllers/Http/CannaGrowController.js
+++ app/Controllers/Http/CannaGrowController.js
@@ -33,7 +33,7 @@ class CannaGrowController {
     async indexItem({request,response,auth}){
         //  try {
               let user =  await auth.getUser()
-            let allItems =await Item.query().where('userId',7).with('tags').fetch()
+            let allItems =await Item.query().where('userId',user.id).with('tags').fetch()
               
               return response.status(200).json({
                   'success': true,
@@ -90,7 +90,7 @@ class CannaGrowController {
     async showItem({request,response,auth,params}){
         //  try {
               let item =await Item.query().where('id',params.id).with('tags').with('store').with('user').with('reviews')
-              .with('avgRating')
+              //.with('avgRating')
               .first()
               return response.status(200).json({
                   'success': true,
