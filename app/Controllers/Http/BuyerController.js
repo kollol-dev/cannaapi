4fe7d193826984cc11f2cb505753eefe107c4d9b
sadek <sadek.hkm@gmail.com>
2019-07-16 06:20:39 +0600

    ad

--- app/Controllers/Http/BuyerController.js
+++ app/Controllers/Http/BuyerController.js
@@ -1,13 +1,21 @@
 'use strict'
 const User = use('App/Models/User');
 const Cannagrow = use('App/Models/Cannagrow');
+const Curt = use('App/Models/Curt');
 const Item = use('App/Models/Item');
 const ItemTag = use('App/Models/ItemTag');
 const ItemReview = use('App/Models/ItemReview');
 const Database = use('Database')
 var _ = require('lodash')
 class BuyerController {
-    // added a comment..
+   async updateQuantity({auth,request}){
+       const uid = await auth.user.id 
+       const data = request.all()
+       console.log('data is', data)
+       return Curt.query().where('id', data.id).where('userId', uid).update({
+           'quantity' : data.quantity
+       }) 
+   }
 
 }
 
--- start/Routes/buyer.js
+++ start/Routes/buyer.js
@@ -1,2 +1,4 @@
 'use strict'
 const Route = use('Route')
+
+Route.post('app/update-quanitity', 'BuyerController.updateQuantity')
