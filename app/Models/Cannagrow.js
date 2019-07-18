cefb5150e1ab08afdce21b1e7fb73084f0cae67a
nazmul <nazmulchowdhury4@gmail.com>
2019-07-18 16:33:04 +0600

    some bugs fixed againdd

--- app/Models/Cannagrow.js
+++ app/Models/Cannagrow.js
@@ -6,13 +6,13 @@ const Database = use('Database')
 class Cannagrow extends Model {
 
     reviews () {
-        return this.hasMany('App/Models/ItemReview', 'id', 'growerId')
+        return this.hasMany('App/Models/ItemReview', 'id', 'growId')
     }
     user () {
         return this.belongsTo('App/Models/User', 'userId', 'id')
     }
     avgRating () {
-        return this.hasOne('App/Models/ItemReview', 'id', 'growerId').select('growId', Database.raw('cast(AVG(rating) as decimal(10,2)) AS averageRating')).groupBy('growId')
+        return this.hasOne('App/Models/ItemReview', 'id', 'growId').select('growId', Database.raw('cast(AVG(rating) as decimal(10,2)) AS averageRating')).groupBy('growId')
       }
     avgPrice () {
         return this.hasOne('App/Models/Item', 'id', 'growId').select('growId', Database.raw('cast(AVG(price) as decimal(10,2)) AS averagePrice')).groupBy('growId')
