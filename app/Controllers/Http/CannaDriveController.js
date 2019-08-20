8ae9f3ab0f7ab030c26f4f653db346dabf0930f6
nazmul <nazmulchowdhury4@gmail.com>
2019-08-20 17:08:42 +0600

    with this oks

--- app/Controllers/Http/CannaDriveController.js
+++ app/Controllers/Http/CannaDriveController.js
@@ -189,7 +189,7 @@ class CannaDriveController {
   }
   async getNewOrder({request,response,auth}){
    
-    let order =  await Order.query().where('status', 'Request for Driver').with('orderdetails').with('buyer').with('seller').fetch();
+    let order =  await Order.query().where('status', 'Request for Driver').with('orderdetails.item').with('buyer').with('seller').fetch();
     return response.status(200).json({
       'success': true,
       "orders": order
