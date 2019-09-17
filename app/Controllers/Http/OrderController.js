e1e2822834eff4aa0711b826899e08c4bfa6058c
Kollol <Kollol>
2019-09-17 19:02:16 +0600

    'werws'd

--- app/Controllers/Http/OrderController.js
+++ app/Controllers/Http/OrderController.js
@@ -10,7 +10,7 @@ const Database = use('Database')
 
 
 // firebase
-// var admin = require('firebase-admin');
+var admin = require('firebase-admin');
 // var serviceAccount = require("./FirebaseAdminSDK_PvtKey/cannaapp-87a30-firebase-adminsdk-2zpyz-cbc3a9713e.json");
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
@@ -57,32 +57,32 @@ class OrderController {
       allCurtInfo.push(ob)
     }
 
-    // let token = await User.query().where('id', sellerUserId.userId).select('app_Token').first()
-    // console.log('token_id', token)
-    // var registrationToken = token.token;
+    let token = await User.query().where('id', sellerUserId.userId).select('app_Token').first()
+    console.log('token_id', token)
+    var registrationToken = token.token;
 
-    // var message = {
-    //   data: {
-    //     score: '850',
-    //     time: '2:45'
-    //   },
-    //   notification: {
-    //     title: data.title,
-    //     body: data.body
-    //   },
-    //   token: registrationToken
-    // };
+    var message = {
+      data: {
+        score: '850',
+        time: '2:45'
+      },
+      notification: {
+        title: data.title,
+        body: data.body
+      },
+      token: registrationToken
+    };
 
-    // // Send a message to the device corresponding to the provided
-    // // registration token.
-    // admin.messaging().send(message)
-    //   .then((response) => {
-    //     // Response is a message ID string.
-    //     console.log('Successfully sent message:', response);
-    //   })
-    //   .catch((error) => {
-    //     console.log('Error sending message:', error);
-    //   });
+    // Send a message to the device corresponding to the provided
+    // registration token.
+    admin.messaging().send(message)
+      .then((response) => {
+        // Response is a message ID string.
+        console.log('Successfully sent message:', response);
+      })
+      .catch((error) => {
+        console.log('Error sending message:', error);
+      });
 
 
     Noti.create({
