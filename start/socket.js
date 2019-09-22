da2f63623e1c24ed8c6bfc2813728960bdcfd73d
Sadek Hossain <sadek.hkm@gmail.com>
2019-09-22 16:37:02 +0600

    testing socket.io server

--- start/socket.js
+++ start/socket.js
@@ -1,7 +1,13 @@
+import { isBoolean } from "util"
+
 const Server = use('Server')
 const io = use('socket.io')(Server.getInstance())
 
 io.on('connection', function (socket) {
   console.log('bangladesh !')
   console.log(socket.id)
+})
+
+io.on('driver_location', (data) => {
+  console.log('driver location lat', data.lat, 'lng', data.lng)
 })
