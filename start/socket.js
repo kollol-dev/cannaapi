3d409e1afbd9d0a65e71bf68e11e408188ef3f41
Sadek Hossain <sadek.hkm@gmail.com>
2019-09-22 15:08:19 +0600

    werse

--- start/socket.js
+++ start/socket.js
@@ -2,5 +2,6 @@ const Server = use('Server')
 const io = use('socket.io')(Server.getInstance())
 
 io.on('connection', function (socket) {
+  console.log('bangladesh !')
   console.log(socket.id)
 })
