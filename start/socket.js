3dfea020921f36d9d75c9fecc835e92e4c73b133
Sadek Hossain <sadek.hkm@gmail.com>
2019-09-23 13:25:59 +0600

    iude

--- start/socket.js
+++ start/socket.js
@@ -8,8 +8,8 @@ io.on('connection', function (socket) {
     console.log('data', data)
     console.log('driver location lat', data.lat, 'lng', data.lng)
 
-    socket.emit('driver_location_from_server', {loc: data})
-    socket.emit('news', { hello: 'world' });
+    io.emit('driver_location_from_server', {loc: data})
+    io.emit('news', { hello: 'world' });
   })
 
 
