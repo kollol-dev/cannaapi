ee31bb44399b1967195ad49ebe815119f35d0ad6
Sadek Hossain <sadek.hkm@gmail.com>
2019-09-24 13:37:08 +0600

    werw

--- start/socket.js
+++ start/socket.js
@@ -4,6 +4,7 @@ const io = use('socket.io')(Server.getInstance())
 io.on('connection', function (socket) {
   console.log('socket id', socket.id)
 
+  console.log('query', socket)
   socket.on('driver_location', (data) => {
     console.log('data', data)
     console.log('driver location lat', data.lat, 'lng', data.lng)
@@ -11,12 +12,22 @@ io.on('connection', function (socket) {
     io.emit('driver_location_from_server', data)
     io.emit('news', { hello: 'world' });
   })
-
-
 })
-
-
 
 io.on('message', (data) => {
   console.log('data', data)
 })
+
+
+// const nsp = io.of('/my-namespace');
+// nsp.on('connection', function (socket) {
+
+//   socket.on('driver_location', (data) => {
+//     console.log('namespace', data)
+    
+//     io.emit('driver_location_from_server', data)
+//     io.emit('news', { hello: 'world' });
+//   })
+
+// });
+// nsp.emit('hi', 'everyone!');
