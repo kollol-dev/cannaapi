d888df5b653f341a690bb97112ac57087188a7ab
Sadek Hossain <sadek.hkm@gmail.com>
2019-09-24 16:30:09 +0600

    werw

--- start/socket.js
+++ start/socket.js
@@ -1,33 +1,36 @@
 const Server = use('Server')
 const io = use('socket.io')(Server.getInstance())
 
-// io.on('connection', function (socket) {
-//   console.log('socket id', socket.id)
+let drivers = []
 
-//   console.log('query', socket)
-//   socket.on('driver_location', (data) => {
-//     console.log('data', data)
-//     console.log('driver location lat', data.lat, 'lng', data.lng)
+io.on('connection', function (socket) {
+  console.log('socket id', socket.id)
 
-//     io.emit('driver_location_from_server', data)
-//     io.emit('news', { hello: 'world' });
-//   })
-// })
-
-// io.on('message', (data) => {
-//   console.log('data', data)
-// })
-
-
-const nsp = io.of('/driver');
-nsp.on('connection', function (socket) {
-
+  console.log('query', socket)
+  // console.log('query', socket._query.driverId)
   socket.on('driver_location', (data) => {
-    console.log('namespace', data)
-    
+    console.log('data', data)
+    console.log('driver location lat', data.lat, 'lng', data.lng)
+
     io.emit('driver_location_from_server', data)
     io.emit('news', { hello: 'world' });
   })
+})
 
-});
+io.on('message', (data) => {
+  console.log('data', data)
+})
+
+
+// const nsp = io.of('/driver');
+// nsp.on('connection', function (socket) {
+
+//   socket.on('driver_location', (data) => {
+//     console.log('namespace', data)
+    
+//     io.emit('driver_location_from_server', data)
+//     io.emit('news', { hello: 'world' });
+//   })
+
+// });
 nsp.emit('hi', 'everyone!');
