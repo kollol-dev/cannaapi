9034eeeee631c2b653c564da040a09a61943108c
Sadek Hossain <sadek.hkm@gmail.com>
2019-10-03 15:34:05 +0600

    testing push notificaiton

--- app/Controllers/Http/OrderController.js
+++ app/Controllers/Http/OrderController.js
@@ -72,7 +72,9 @@ class OrderController {
 
     let token = await User.query().where('id', sellerUserId.userId).select('app_Token').first()
     console.log('token_id', token)
+    token = JSON.parse(token)
     var registrationToken = token.token;
+    console.log('reg_token', registrationToken)
 
     var message = {
       data: {
